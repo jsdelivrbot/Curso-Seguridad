418b15a643fa9c62e3c093968c929f3b54f1746d
Manu <manumuve@gmail.com>
2018-10-23 10:21:50 +0200

    Update js-remoto.js

--- js-remoto.js
+++ js-remoto.js
@@ -1,3 +1,3 @@
 // Acceder a través de https://cdn.rawgit.com/manumuve/Curso-Seguridad/master/js-remoto.js
-console.log("Hola Mundo!"); 
+console.log("Hola! Soy un código remoto ejecutándome en tu aplicación"); 
 //$("#login-modal").modal("show");
